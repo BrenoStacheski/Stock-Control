44dcd1b17be1dacb9bd557d81a69d05331470238
Breno Stacheski <brenosttc@gmail.com>
2023-09-28 22:31:30 -0300

    [feat]: implementing login and sign-up reactive forms

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    senha: [null, Validators.required]
+  })
+
+  signupForm = this.formBuilder.group({
+    nome: [null, Validators.required],
+    email: [null, Validators.required],
+    senha: [null, Validators.required],
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  onSubmitLoginForm(): void {
+    console.log('DADOS DO FORMULÁRIO DE LOGIN', this.loginForm.value)
+  }
+
+  onSubmitSignupForm(): void {
+    this.signupForm.reset();
+    console.log('DADOS DO FORMULÁRIO DE SIGNup', this.signupForm.value)
+  }
 }
