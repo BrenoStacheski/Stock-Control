2e9287167a4468a40079238810d9394a746c8354
Breno Stacheski <brenosttc@gmail.com>
2023-10-09 22:06:38 -0300

    [feat]: implementing products route and authentication to access it

--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { ProductsDataTransferService } from './../../../../shared/services/products/products-data-transfer.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-products-home',
   templateUrl: './products-home.component.html',
   styleUrls: []
 })
-export class ProductsHomeComponent {
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsList: Array<GetAllProductsResponse> = [];
 
+  constructor(
+    private productsService: ProductsService,
+    private productsDataTransferService: ProductsDataTransferService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getServiceProductData();
+  }
+
+  getServiceProductData(): void {
+    const productsLoaded = this.productsDataTransferService.getProductsData();
+
+    if (productsLoaded.length > 0) {
+      this.productsList = productsLoaded;
+    } else {
+      this.getAPIProductsData();
+    }
+  }
+
+  getAPIProductsData(): void {
+    this.productsService.getAllProducts().pipe(
+      takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsList = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500
+          })
+          this.router.navigate(['/dashboard']);
+        }
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
